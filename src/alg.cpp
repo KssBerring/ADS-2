e79147f84b37f624adc6402613a96a8eb9c625c7
KssBerring <160275047+KssBerring@users.noreply.github.com>
2024-03-29 11:04:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,6 +4,7 @@
 
 
 double pown(double value, uint16_t n) {
+ 
  if (n == 0) {
         return 1;
     } 
@@ -13,6 +14,7 @@ double pown(double value, uint16_t n) {
 }
 
 uint64_t fact(uint16_t n) {
+ 
  if (n == 0) {
         return 1;
     } 
@@ -22,21 +24,24 @@ uint64_t fact(uint16_t n) {
 }
 
 double calcItem(double x, uint16_t n) {
+ 
   return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
+ 
   double result = 1.0; 
-    for (uint16_t n = 1; n <= count; ++n) {
+    for (uint16_t n = 1; n < count; ++n) {
         result += calcItem(x, n);
     }
     return result;
 }
 
 double sinn(double x, uint16_t count) {
+ 
  double result = 0;
 int sign = 1;
-for (uint16_t i = 1; i <= count*2; ++i) {
+for (uint16_t i = 1; i < count*2; ++i) {
     if ((i / 2) != 0) {
         result += sign * calcItem(x, i);
         sign *= -1;
@@ -46,9 +51,10 @@ return result;
 }
 
 double cosn(double x, uint16_t count) {
+ 
  double result = 0;
 int sign = 1;
-for (uint16_t i = 0; i <= count*2; ++i) {
+for (uint16_t i = 0; i < count*2; ++i) {
     if ((i / 2) == 0) {
         result += sign * calcItem(x, i);
         sign *= -1;
