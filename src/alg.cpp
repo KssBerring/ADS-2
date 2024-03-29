0c374603c2891a707bcf743350a2e53f99ac54ff
KssBerring <160275047+KssBerring@users.noreply.github.com>
2024-03-29 11:01:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -27,28 +27,32 @@ double calcItem(double x, uint16_t n) {
 
 double expn(double x, uint16_t count) {
   double result = 1.0; 
-    for (uint16_t n = 1; n < count; ++n) {
+    for (uint16_t n = 1; n <= count; ++n) {
         result += calcItem(x, n);
     }
     return result;
 }
 
 double sinn(double x, uint16_t count) {
-  double result = 0;
-    int sign = 1;
-    for (uint16_t i = 0; i < count; ++i) {
-        result += sign * (pown(x, 2 * i + 1) / fact(2 * i + 1));
+ double result = 0;
+int sign = 1;
+for (uint16_t i = 1; i <= count*2; ++i) {
+    if ((i / 2) != 0) {
+        result += sign * calcItem(x, i);
         sign *= -1;
     }
-    return result;
+}
+return result;
 }
 
 double cosn(double x, uint16_t count) {
-  double result = 0;
-    int sign = 1;
-    for (uint16_t i = 0; i < count; ++i) {
-        result += sign * (pown(x, 2 * i) / fact(2 * i));
+ double result = 0;
+int sign = 1;
+for (uint16_t i = 0; i <= count*2; ++i) {
+    if ((i / 2) == 0) {
+        result += sign * calcItem(x, i);
         sign *= -1;
     }
-    return result;
+}
+return result;
 }
