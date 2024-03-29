2c961176d1acec855c9b5c5bc110e18535385c7e
KssBerring <160275047+KssBerring@users.noreply.github.com>
2024-03-29 14:43:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -27,16 +27,16 @@ double calcItem(double x, uint16_t n) {
 
 double expn(double x, uint16_t count) {
  double result = 1.0; 
- for (uint16_t n = 1; n <= count; ++n) {
-  result += calcItem(x, n);
+ for (uint16_t i = 1; i <= count; ++i) {
+  result += calcItem(x, i);
  }
  return result;
 }
 
 double sinn(double x, uint16_t count) {
  double result = x;
- for (uint16_t n = 2; n <= count; ++n) {
-  result += pown((-1.0), i - 1) * calcItem(x, (2*n)-1);
+ for (uint16_t i = 2; i <= count; ++i) {
+  result += pown((-1.0), i - 1) * calcItem(x, (2*i)-1);
  }
  return result;
 }
