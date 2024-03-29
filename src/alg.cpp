029d96444b32aa3909f86a72da3ed35084ac9cdc
KssBerring <160275047+KssBerring@users.noreply.github.com>
2024-03-29 11:53:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,57 +4,51 @@
 
 
 double pown(double value, uint16_t n) {
- 
  if (n == 0) {
-        return 1;
-    } 
+  return 1;
+ } 
  else {
-        return value * pown(value, n - 1);
-    }
+  return value * pown(value, n - 1);
+ }
 }
 
 uint64_t fact(uint16_t n) {
- 
  if (n == 0) {
-        return 1;
-    } 
+  return 1;
+ } 
  else {
-        return n * fact(n - 1);
-    }
+  return n * fact(n - 1);
+ }
 }
 
 double calcItem(double x, uint16_t n) {
- 
   return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
- 
-  double result = 1.0; 
-    for (uint16_t n = 1; n <= count; ++n) {
-        result += calcItem(x, n);
-    }
-    return result;
+ double result = 1.0; 
+ for (uint16_t n = 1; n <= count; ++n) {
+  result += calcItem(x, n);
+ }
+ return result;
 }
 
 double sinn(double x, uint16_t count) {
  double result = 0.0;
  int sign = 1;
  for (uint16_t n = 1; n <= count; ++n) {
-        result += sign * calcItem(x, 2*n-1);
-        sign *= -1;
+  result += sign * calcItem(x, 2*n-1);
+  sign *= -1;
  }
  return result;
 }
 
 double cosn(double x, uint16_t count) {
-     double result = 1.0;
-    int sign = -1;
-    for (uint16_t i = 2; i <= count; ++i) {
-
-            result += sign * calcItem(x, 2*i-2);
-            sign *= -1;
-        
-    }
-    return result;
+ double result = 1.0;
+ int sign = -1;
+ for (uint16_t i = 2; i <= count; ++i) {
+  result += sign * calcItem(x, 2*i-2);
+  sign *= -1;
+ }
+ return result;
 }
